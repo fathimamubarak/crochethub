61e7189ee28cf48b98faee1f51f0b2ced3629be9
Fathima Mubarak <fathimamubarak7869@gmail.com>
2026-06-24 17:12:41 +0530

    Created first CrochetHub React page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>🧶 CrochetHub</h1>
+
+      <h2>Connecting Crocheters and Customers</h2>
+
+      <p>
+        A platform to manage custom crochet orders,
+        track progress and connect creators with customers.
+      </p>
+    </div>
+  );
+}
+
+export default App;
